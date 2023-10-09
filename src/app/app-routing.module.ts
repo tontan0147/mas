6177f18379c6a0947f5d97c80cbb7e47c55ce5bd
tontan0147 <tontan.mok38@gmail.com>
2023-10-09 21:49:30 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {CrudComponent} from "./crud/crud.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',component : HomeComponent
+  },{
+    path: 'login',component : CrudComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CrudComponent } from './crud/crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
